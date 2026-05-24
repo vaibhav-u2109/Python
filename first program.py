9044cb4847bfa279c6530497e6ebe902d5ce3341
Vaibhav Upadhyay <vaibhavupadhyay424@gmail.com>
2026-05-24 13:21:03 +0530

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,27 @@
+# a=10
+# b=78
+# sum=a+b
+# print(sum)  
+
+
+# a=10
+# b=78
+# print(a==b)
+# print(a!=b)
+# a=10
+# b=78
+# print(not(a==b))
+# print((a==b)and(a<b))
+# print((a==b)or(a<b))
+# name=input("enter your name ")
+# print("hello",name)
+# age=int(input("enter your age"))
+# print("your age is",age)
+# marks=float(input("enter your marks"))
+# print("your marks is",marks)
+# 
+# area=int(input("enter the area"))
+# print("the area of aqaure is",area*area)
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+print(a>b)
